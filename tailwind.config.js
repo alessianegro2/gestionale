55a3c96d2c2b23be0fd1f710771981b6f300eb43
Alessia Negro <negro.alessia06@gmail.com>
2025-05-07 21:05:25 +0200

    spostato layout.txs di nuovo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx}", // Corretto percorso per app
-    "./src/components/**/*.{js,ts,jsx,tsx}", // Corretto percorso per components
-    "./src/styles.css", // Assicuriamo di includere anche il file CSS
+    "./src/app/**/*.{js,ts,jsx,tsx}", 
+    "./src/components/**/*.{js,ts,jsx,tsx}", 
+    "./src/app/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
