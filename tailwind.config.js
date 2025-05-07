6516196c2768a873ef3134e14b656b2dbd0dad09
alessia negro <147917950+alessianegro2@users.noreply.github.com>
2025-05-07 20:10:43 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Corretto percorso per app
+    "./src/components/**/*.{js,ts,jsx,tsx}", // Corretto percorso per components
+    "./src/styles.css", // Assicuriamo di includere anche il file CSS
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
